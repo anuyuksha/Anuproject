f9b343241d715c95df421ffff20a5b61110b78b7
anuyuksha <agsyona@gmail.com>
2015-11-26 23:30:30 +0530

    code.

--- Anu/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ Anu/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -35,5 +35,10 @@ namespace WindowsFormsApplication1
             Form2 p2 = new Form2();
             p2.ShowDialog();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
